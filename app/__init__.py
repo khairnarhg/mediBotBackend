0435f2ea09816a27eb030c56736a7d6491a25487
Harshit Kandpal <harshitheroh5@gmail.com>
2025-02-13 00:01:26 +0530

    2nd commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask_cors import CORS
 from app.routes import configure_routes
 from app.db import initialize_vectordbs  # Import the initialization function
 
 def create_app():
     app = Flask(__name__)
+
+    # Enable CORS for all origins
+    CORS(app)
 
     # Upload folder for PDFs
     app.config['UPLOAD_FOLDER'] = 'app/static/uploads'
